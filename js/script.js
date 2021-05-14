b95e8f130bba9d576b1846197375d92f696cd325
Andrey <fail4iks@gmail.com>
2021-05-14 23:51:27 +0300

    modal modification

--- js/script.js
+++ js/script.js
@@ -58,25 +58,25 @@ window.addEventListener('DOMContentLoaded', () => {
     //3) функция которая занимается обновлением нашего таймера
     const deadline = '2021-04-13'; //дата обратного отсчета
     function getTimeRemainning(endtime) { //функция определяющая разницу между датами
-      const t = Date.parse(endtime) - Date.parse(new Date()), //преобразуем даты в число (количество миллисекунд). 
+        const t = Date.parse(endtime) - Date.parse(new Date()), //преобразуем даты в число (количество миллисекунд). 
             //делим количество миллисекунд на кол-во млсек в 1 дне
-           days = Math.floor(t / (1000 * 60 * 60 * 24)), //определить количество дней отображаемых в обратном таймере Math.floor округление до ближайшего целого
-           // 1000*60*60*24 - столько в сутках миллисекунд
-           hours = Math.floor((t / (1000 * 60 * 60) % 24)), //определяем количество целых часов до конца.
-           //1000*60*60 столько миллисекунд в часе (1000 милли секунд в секунде). t / (1000 * 60 * 60) - общее количество часов которое осталось, те там может быть и  150 и 300, нам это не нужно.
-           // % 24 - деление на 24 и возвращение остатка от деления. 
-           minutes = Math.floor((t / 1000 /60) % 60),
-           seconds = Math.floor((t / 1000) % 60);
-           //Для того чтобы вышеперечисленные переменные были видны и снаружи, необходимо использовать return
-           return { //возвращаем из функции объект
-               'total': t,
-               'days': days,
-               'hours': hours,
-               'minutes': minutes,
-               'seconds': seconds
-           };
+            days = Math.floor(t / (1000 * 60 * 60 * 24)), //определить количество дней отображаемых в обратном таймере Math.floor округление до ближайшего целого
+            // 1000*60*60*24 - столько в сутках миллисекунд
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)), //определяем количество целых часов до конца.
+            //1000*60*60 столько миллисекунд в часе (1000 милли секунд в секунде). t / (1000 * 60 * 60) - общее количество часов которое осталось, те там может быть и  150 и 300, нам это не нужно.
+            // % 24 - деление на 24 и возвращение остатка от деления. 
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        //Для того чтобы вышеперечисленные переменные были видны и снаружи, необходимо использовать return
+        return { //возвращаем из функции объект
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
     }
-    
+
     function getZero(num) { //функция которая добавляет ноль впереди числу, если оно меньше 10
         if (num >= 0 && num < 10) { //число должно быть больше или равно 0 и меньше чем двухзначное 10
             return `0${num}`; //если условие произошло, то из функции возвращаем модифицированное значение
@@ -87,11 +87,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function setClock(selector, endtime) { //установка часов
         const timer = document.querySelector(selector), //вместо selector можно указать и .timer, но мы дклаем общую функцию
-              days = timer.querySelector('#days'),
-              hours = timer.querySelector('#hours'),
-              minutes = timer.querySelector('#minutes'),
-              seconds = timer.querySelector('#seconds'),
-              timeInterval = setInterval(updateClock, 1000); //обновление таймера кажду секунду
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000); //обновление таймера кажду секунду
 
         updateClock(); //вызвать функцию вручную, чтобы цифры счетчика встали сразу корректные, а если этого не сделать сначала появятся цифры по умолчанию, и только через 1 сек счетчик.
 
@@ -102,70 +102,92 @@ window.addEventListener('DOMContentLoaded', () => {
             hours.innerHTML = getZero(t.hours);
             minutes.innerHTML = getZero(t.minutes);
             seconds.innerHTML = getZero(t.seconds);
-  
+
             if (t.total <= 0) { //если время вышло и оно идет в отрицательную сторону, то таймер не обновляем
                 clearInterval(timeInterval);
             }
 
         }
-         
+
     }
     setClock('.timer', deadline); //первая переменная это selector
 
     //Modal
-/*     Первым делом необходимо в HTML найти кнопки/триггеры которые будут вызывать модальное окно,
-    они могут иметь разные классы, теги и чтобы их как то объединить, пометить им назначают data атрибуты
-    <button data-modal class="btn btn_white">Связаться с нами</button>
-    Также вешаем data атрибут на закрытие модального окна
-     <div data-close class="modal__close">&times;</div> */
-    
+    /*     Первым делом необходимо в HTML найти кнопки/триггеры которые будут вызывать модальное окно,
+        они могут иметь разные классы, теги и чтобы их как то объединить, пометить им назначают data атрибуты
+        <button data-modal class="btn btn_white">Связаться с нами</button>
+        Также вешаем data атрибут на закрытие модального окна
+         <div data-close class="modal__close">&times;</div> */
+
     //Триггер для модального окна это будет кнопка "Связаться с Нами"
     const modalTrigger = document.querySelectorAll('[data-modal]'), //атрибут прописывается в квадратных скобках. Находим кнопки.
-          modal = document.querySelector('.modal'), //переменная отвечающая за само модальное окно
-          modalCloseBtn = document.querySelector('[data-close]'); //кнопка отвечающая за закрытие окна
+        modal = document.querySelector('.modal'), //переменная отвечающая за само модальное окно
+        modalCloseBtn = document.querySelector('[data-close]'); //кнопка отвечающая за закрытие окна
 
-          
 
- /*    modalTrigger.addEventListener('click', () => { //при клике на кнопку "Связаться с нами" выходит модальное окно
-        modal.classList.add('show');
-        modal.classList.remove('hide');
-        document.body.style.overflow = 'hidden'; //запрет прокрутки страницы при открытом модальном окне
-       });
 
-    modalCloseBtn.addEventListener('click', () => { //обратная операция, при нажатии на крестить в модальном окне, оно закрывается
-        modal.classList.add('hide'); //добавить класс скрытия
-        modal.classList.remove('show'); //удалить класс показа
-        document.body.style.overflow = ''; //при закрытии модального окна, восстановить скрол страницы
-    });   */ 
+    /*    modalTrigger.addEventListener('click', () => { //при клике на кнопку "Связаться с нами" выходит модальное окно
+           modal.classList.add('show');
+           modal.classList.remove('hide');
+           document.body.style.overflow = 'hidden'; //запрет прокрутки страницы при открытом модальном окне
+          });
+
+       modalCloseBtn.addEventListener('click', () => { //обратная операция, при нажатии на крестить в модальном окне, оно закрывается
+           modal.classList.add('hide'); //добавить класс скрытия
+           modal.classList.remove('show'); //удалить класс показа
+           document.body.style.overflow = ''; //при закрытии модального окна, восстановить скрол страницы
+       });   */
 
 
     // второй способ вызова модального окна, с помощью переключения класса:
     modalTrigger.forEach(btn => { //необходимо кнопки перебрать тк мы не можем на псевдомассив повесить обработчик события
         //перебор также нужен и для первого способа, чтобы повесить обработчик события на все кнопки
-         btn.addEventListener('click', () => { //при клике на кнопку "Связаться с нами" выходит модальное окно
-             modal.classList.toggle('show'); //переключить класс show (тк класса еще нет, то он его автоматически допишет)
-             document.body.style.overflow = 'hidden'; //запрет прокрутки страницы при открытом модальном окне
-            });
-       }); 
+        btn.addEventListener('click', openModal); //при клике на кнопку "Связаться с нами" выходит модальное окно
+    });
 
-       //если какой то участок кода повтораяется два и более раз, то его необходимо вынести в отдельную функцию
-       function closeModal() { //функция закрытия модального окна
+    //если какой то участок кода повтораяется два и более раз, то его необходимо вынести в отдельную функцию
+    function openModal() {
+        modal.classList.toggle('show'); //переключить класс show (тк класса еще нет, то он его автоматически допишет)
+        document.body.style.overflow = 'hidden'; //запрет прокрутки страницы при открытом модальном окне
+        clearInterval(modalTimerId); //очищаем и останавливаем таймер, если пользователь сам уже открывал модальное окно.
+    }
+
+    function closeModal() { //функция закрытия модального окна
         modal.classList.toggle('show'); // класс уже есть у окна, те при нажатии на кнопку класс удалится.
         document.body.style.overflow = ''; //при закрытии модального окна, восстановить скрол страницы
-       }
+    }
 
     modalCloseBtn.addEventListener('click', closeModal); //обратная операция, при нажатии на крестить в модальном окне, оно закрывается
-        
+
     modal.addEventListener('click', (e) => { //e.target это то, куда кликнул пользователь
-    if (e.target === modal) { //при клике в подложку (modal) окно также закрывается
-        closeModal(); // в данном случае пишем со скобками, те условие выполнилось, функция выполняется
-    }
+        if (e.target === modal) { //при клике в подложку (modal) окно также закрывается
+            closeModal(); // в данном случае пишем со скобками, те условие выполнилось, функция выполняется
+        }
     });
 
     document.addEventListener('keydown', (e) => { //при нажатии на кнопку ESC окно также закрывается
         //кода кнопок можно посмотреть на сайте https://keycode.info/
-    if (e.code === "Escape" && modal.classList.contains('show')) { //чтобы каждый раз при нажатии на ESC не вызывалась функция прописываем условие, что вызвать только когда модальное окно показано
-    closeModal();
-    }
+        if (e.code === "Escape" && modal.classList.contains('show')) { //чтобы каждый раз при нажатии на ESC не вызывалась функция прописываем условие, что вызвать только когда модальное окно показано
+            closeModal();
+        }
     });
+
+    //044 Модификации модального окна. 
+    //Всплытие окна при долистывании страницы до конца и при прошестви некоторого времени
+
+    const modalTimerId = setTimeout(openModal, 60000); //через 6 сек откроется модальное окно, если он ранее его не открывал
+    
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            //если условие выполняется, то означает, что пользователь долистал до конца.
+            //свойство pageYOffset отслеживает сколько px отлистал поль-ль по оси Y (прокрученная часть)
+            //свойство scrollHeight получение полной высоты элемента с учетом прокрутки которая была сверху
+            //clientHeight высота клиента, те видимой части окна
+        openModal();
+        window.removeEventListener('scroll', showModalByScroll); //как только пол-ль долистал один раз старцницу до конца, всплывет модальное окно и все, больше всплывать не будет.
+        }
+    }
+    window.addEventListener('scroll', showModalByScroll); //при долистывании страницы до конца появляется модальное окно
+    
+    
 });
