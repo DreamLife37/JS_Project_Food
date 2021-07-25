55e7215f5854f6bcf9c7dfee7b1019142d64cdd9
Andrey <fail4iks@gmail.com>
2021-07-25 08:31:40 +0300

    calculator. 066

--- js/script.js
+++ js/script.js
@@ -783,4 +783,97 @@ window.addEventListener('DOMContentLoaded', () => {
       }); */
 
 
+    // Calculator. Создание калькулятора часть 1. 066
+
+//в html добавляем уникальный идентификатор female и male, также пропишем каждому уровню активности data атрибут (data-ratio) с определенным значением.
+
+    const result = document.querySelector('.calculating__result span'); //находим класс и внутри него получаем span (текстовое поле)
+    let sex = 'female',
+        height, weight, age,
+        ratio = 1.375;
+
+        //В 1-ом и 3-ем блоке будет одна функция, которая будет брать значение из div. А вторая функция будет работать с input во втором блоке.
+    function calcTotal() {
+        //если нет пола или роста или веса или возраста или коэфициента, то расчет производить не будем
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____';
+            return; //return прерывает дальнейшие действия
+        }
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio); //формула для женщин
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio); //формула для мужщин
+        }
+    }
+
+    calcTotal();
+
+    //Функция по получению статических данных: пол и физическая активность
+    //С такой записью функции будет баг при клике мимо кнопки, на подложку
+    /*    function getStaticInformation(parentSelector, activeClass) {
+           const elements = document.querySelectorAll(`${parentSelector} div`);
+           //делигирование событий, на родителя вешаем обработчик события:
+           document.querySelector(parentSelector).addEventListener('click', (e) => { //если у блока по поторому мы кликнули есть атрибут data-ratio, то изменяем переменную ratio
+               if (e.target.getAttribute('data-ratio')) {
+                   ratio = +e.target.getAttribute('data-ratio');
+               } else {
+                   sex = e.target.getAttribute('id');
+               }
+               console.log(ratio, sex)
+               //перебираем все элементы и удаляем класс активности, а объекту события, по которому кликнули назначаем класс активности
+               elements.forEach(elem => {
+                   elem.classList.remove(activeClass)
+               })
+               e.target.classList.add(activeClass);
+               calcTotal();
+           });
+       } */
+
+    //исправление бага при клике на подложку кнопок,это происходит из-за того, что мы используем дилигирование событий (запись выше), лучше вместо этого использовать простое навешивание событий.Для этого мы перебираем все элементы внутри и на каждый из них вешаем обработчик события
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`); //получение внутри родителя всех div
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                //если у блока по поторому мы кликнули есть атрибут data-ratio, то изменяем переменную ratio
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio');
+                } else {
+                    sex = e.target.getAttribute('id');
+                }
+                console.log(ratio, sex)
+                //перебираем все элементы и удаляем класс активности, а объекту события, по которому кликнули назначаем класс активности
+                elements.forEach(elem => { 
+                    elem.classList.remove(activeClass)
+                })
+                e.target.classList.add(activeClass);
+                calcTotal();
+            });
+        });
+    }
+
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+        input.addEventListener('input', () => {
+            switch (input.getAttribute('id')) { //если id равно росту, то записываем значение в рост и тд.
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+            calcTotal(); //фунция конечного расчета должна вызываться каждый раз когда происходит какое то изменение на странице
+        });
+
+    }
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
 });
