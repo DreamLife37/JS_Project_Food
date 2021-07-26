ca4e01f5cc86a48c066beae48071d50829c6f462
Andrey <fail4iks@gmail.com>
2021-07-26 08:15:22 +0300

    calculator ver2. 067

--- js/script.js
+++ js/script.js
@@ -785,14 +785,47 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Calculator. Создание калькулятора часть 1. 066
 
-//в html добавляем уникальный идентификатор female и male, также пропишем каждому уровню активности data атрибут (data-ratio) с определенным значением.
+    //в html добавляем уникальный идентификатор female и male, также пропишем каждому уровню активности data атрибут (data-ratio) с определенным значением.
 
     const result = document.querySelector('.calculating__result span'); //находим класс и внутри него получаем span (текстовое поле)
-    let sex = 'female',
-        height, weight, age,
-        ratio = 1.375;
 
-        //В 1-ом и 3-ем блоке будет одна функция, которая будет брать значение из div. А вторая функция будет работать с input во втором блоке.
+    let sex, height, weight, age, ratio
+
+    //067 Если в localStorage есть значения взять их оттуда, иначе установить дефолтные
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female';
+        localStorage.setItem('sex', 'female');
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
+        ratio = '1.375';
+        localStorage.setItem('ratio', '1.375');
+    }
+
+    //067 Функция по установке класса активности из localStorage
+    function initLocalSettings(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+            //добавляем условия, чтобы не писать 2 функции
+            if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+                elem.classList.add(activeClass);
+            } 
+            if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                elem.classList.add(activeClass);
+            }
+        })
+    }
+    //Функция сначала перебирает пол, далее вызываем и перебираем активность
+    //Тк мы обращаемся к блокам, то указываем div 
+    initLocalSettings('#gender div', 'calculating__choose-item_active');
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
+    //В 1-ом и 3-ем блоке будет одна функция, которая будет брать значение из div. А вторая функция будет работать с input во втором блоке.
     function calcTotal() {
         //если нет пола или роста или веса или возраста или коэфициента, то расчет производить не будем
         if (!sex || !height || !weight || !age || !ratio) {
@@ -830,19 +863,23 @@ window.addEventListener('DOMContentLoaded', () => {
        } */
 
     //исправление бага при клике на подложку кнопок,это происходит из-за того, что мы используем дилигирование событий (запись выше), лучше вместо этого использовать простое навешивание событий.Для этого мы перебираем все элементы внутри и на каждый из них вешаем обработчик события
-    function getStaticInformation(parentSelector, activeClass) {
-        const elements = document.querySelectorAll(`${parentSelector} div`); //получение внутри родителя всех div
+    function getStaticInformation(selector, activeClass) {
+        //const elements = document.querySelectorAll(`${parentSelector} div`); //получение внутри родителя всех div
+        const elements = document.querySelectorAll(selector);
         elements.forEach(elem => {
             elem.addEventListener('click', (e) => {
                 //если у блока по поторому мы кликнули есть атрибут data-ratio, то изменяем переменную ratio
                 if (e.target.getAttribute('data-ratio')) {
                     ratio = +e.target.getAttribute('data-ratio');
+                    //067 Сохранение выбранных пользователем параметров в LocalStorage
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
                 } else {
                     sex = e.target.getAttribute('id');
+                    localStorage.setItem('sex', e.target.getAttribute('id')); //067
                 }
                 console.log(ratio, sex)
                 //перебираем все элементы и удаляем класс активности, а объекту события, по которому кликнули назначаем класс активности
-                elements.forEach(elem => { 
+                elements.forEach(elem => {
                     elem.classList.remove(activeClass)
                 })
                 e.target.classList.add(activeClass);
@@ -852,12 +889,19 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    getStaticInformation('#gender', 'calculating__choose-item_active');
-    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    getStaticInformation('#gender div', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
 
     function getDynamicInformation(selector) {
         const input = document.querySelector(selector);
         input.addEventListener('input', () => {
+            //067 Проверка на ввод не числа
+            if (input.value.match(/\D/g)) {
+                input.style.border = '2px solid red'; //обвести поле красным
+            } else {
+                input.style.border = 'none';
+            }
+
             switch (input.getAttribute('id')) { //если id равно росту, то записываем значение в рост и тд.
                 case 'height':
                     height = +input.value;
